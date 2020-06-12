3dfc3eeff36c54f860c59b4d52cac3d972105fa2
yuichiro watanabe <yuuichiro1139@gmail.com>
2020-06-12 17:15:45 +0900

    add

---
+++ app/javascript/chat/index.jsx
@@ -0,0 +1,40 @@
+// app/javascript/chat/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger'
+import ReduxPromise from 'redux-promise';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import App from './components/app';
+import messagesReducer from './reducers/messages_reducer';
+
+const chatContainer = document.getElementById('chat_app');
+const channels = JSON.parse(chatContainer.dataset.channels).map(channel => channel.name)
+
+const initialState = {
+  messages: [],
+  channels: channels // TODO: get that from Rails DB.
+};
+
+const reducers = combineReducers({
+  messages: messagesReducer,
+  channels: (state = null, action) => state
+});
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger, ReduxPromise));
+const store = createStore(reducers, initialState, middlewares);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/channels/:channel" component={App} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  chatContainer
+);
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+Channel.destroy_all
+User.destroy_all
+
+puts 'User creating'
+User.create(email:'test1@gmail.com', password: 123456)
+User.create(email:'test2@gmail.com', password: 123456)
+puts 'Done'
+
+puts 'Channel creating'
+
+Channel.create!(name: 'general')
+Channel.create!(name: 'React')
+Channel.create!(name: 'Rails')
+
+puts 'Done'
+
+puts 'Message creating'
+Message.create!(user: User.first, channel: Channel.first, content: 'test1')
+Message.create!(user: User.last, channel: Channel.first, content: 'test2')
+puts 'Done'
